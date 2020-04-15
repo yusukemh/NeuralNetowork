c9ab9fce671afea1ae5c4b5401422f5ec01d107a
Yusuke Hatanaka <yusukemh@hawaii.edu>
2020-04-15 13:38:44 -1000

    before copy from python notebook

--- NN_Driver.py
+++ NN_Driver.py
@@ -1,14 +1,15 @@
 import numpy as np
 #import NN_Class
 import NeuralNetwork
-N = 3
+N = 100
 D = 5
-X = np.random.rand(N, D) # NxD matrix with N = 100, D = 18
-Y = np.random.rand(N) # length N vector
+X = np.random.rand(N, D) # NxD matrix
+Y = np.zeros(N) # length N vector
 
-model = NeuralNetwork.NNClassifier([10,10])
+model = NeuralNetwork.NNClassifier([6,10])
 model.fit(X, Y)
-
+a = np.array([1,2,3])
+print(np.multiply(4,a))
 
 
 
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,27 +1,15 @@
 import numpy as np
 
 class NNClassifier():
-    def __init__(self, hidden_layer_sizes):
+    def __init__(self, hidden_layer_sizes, learning_rate = 0.01):
         self.hidden_layer_sizes = hidden_layer_sizes
+        self.learning_rate = learning_rate
         self.n_hidden_layers = len(hidden_layer_sizes)
         self.n_layers = self.n_hidden_layers + 2
         self.W = {}
+        self.Z = {}
+        self.H = {}
         self.layer_sizes = []
-        #Randomly initialize the weights.
-        #Since the input dimension is unknown until x is passed at fit(), only initialize
-        #   weights from the second layer to the output layer.
-        '''
-        ===
-        dL_dz1 = # N x 100
-        dW1 = np.dot(X.T, dL_dz1) # 801 x N dot N x 100 = 801 x 100 //sum N of products
-        '''
-        for i in range(1, self.n_hidden_layers):#leave room for the weight for the first layer
-            self.W[i] = np.random.rand(hidden_layer_sizes[i-1] + 1, hidden_layer_sizes[i]) / 100 
-            # -> (layers[i]+1) x layers[i+1] matrix: bias term included
-            
-        #add the weight matrix for the last layer: bias term included
-        self.W[self.n_hidden_layers] = np.random.rand(hidden_layer_sizes[-1] + 1, 1) / 100
-        #-> (layers[i]+1) x 1 matrix
         
             
     def fit(self, X, Y):
@@ -29,36 +17,60 @@ class NNClassifier():
         #Input: features x: NxD matrix
         #Output: labels y, a length N vector of 0s, 1s
         
-        #first, add the weight matrix for the layer btwn the input and the first layer
-        self.W[0] = np.random.rand(X.shape[1] + 1,  self.hidden_layer_sizes[0]) / 100
-        # -> (D + 1) x hidden_layer_sizes[0] matrix: bias term included
+        #preprocess
+        Y = Y.reshape(100,-1)
+        #incorporate the input and output layers
+        self.layer_sizes.append(X.shape[1])
+        for size in self.hidden_layer_sizes:
+            self.layer_sizes.append(size)
+        self.layer_sizes.append(1)
         
-        n = X.shape[0]
-        bias = np.ones((n,1))
+        #initialize the weight randomly. Allow negative values
+        for i in range(self.n_layers - 1):
+            self.W[i] = (np.random.rand(self.layer_sizes[i] + 1, self.layer_sizes[i+1]) - 0.5) / 100
         
-        for i in range(self.n_hidden_layers + 1):
+        '''
+        for i in range(self.n_layers -1):
             print(self.W[i].shape)
             print(self.W[i])
-            print('\n')
+            print()
+        '''
         
-        #forward propagation
-        Z = {}
-        H = {}
-        #consider the input layer as Z[0] for simplisity.
-        Z[0] = X
-        #do not apply activation for the input layer, but add a column of one's
-        H[0] = np.hstack((Z[0], bias))
+        n = X.shape[0]
+        column = np.ones((n,1))
         
-        #propagate through each layer
-        for i in range(1, self.n_hidden_layers + 1): #1,2,..., n_hidden_layers
-            #calculate the sum
-            Z[i] = np.dot(H[i-1], self.W[i-1])
-            #add column
-            H[i] = np.hstack((self.relu(Z[i]), bias))
-        Yhat = np.dot(H[self.n_hidden_layers], self.W[self.n_hidden_layers])
+        #FORWARD PROPAGATION
+        self.Z[0] = X   #treat the input layer as Z[0]
+        self.H[0] = np.hstack((self.Z[0], column)) #add extra culumn
+        
+        for i in range(1, self.n_layers - 1):#Calculate hidden layers
+            self.Z[i] = np.dot(self.H[i-1], self.W[i-1])
+            self.H[i] = np.hstack((self.relu(self.Z[i]), column))
+        Yhat = np.dot(self.H[self.n_layers - 2], self.W[self.n_layers - 2])
         Yhat = self.sigmoid(Yhat)
-        print(Yhat)
-        pass
+        
+        
+        #BACK PROPAGATION
+        dLdz = Yhat - Y# (N, 1)
+        dLdw = np.dot(np.transpose(dLdz), self.H[self.n_layers - 2]) #(1,N) x (N, |H_top|)
+        
+        G = {}
+        G[self.n_layers - 2] = np.transpose(dLdw)
+        
+        for i in range(self.n_layers - 3, -1, -1): #n_layers - 3, n_layer - 4, ... 0
+            dLdz = np.dot(dLdz, np.transpose(self.W[i+1][:-1,:]))
+            dLdz = np.multiply(dLdz, self.relu(self.Z[i+1]))
+            dLdw = np.dot(np.transpose(dLdz), self.H[i])
+            G[i] = np.transpose(dLdw)
+        
+        #Use the gradient to update the weights
+        for i in range(self.n_layers-1):
+            print("updating weight:", self.W[i].shape)
+            self.W[i] = self.W[i] - np.multiply(self.learning_rate, G[i])
+            print(np.multiply(self.learning_rate, G[i]).shape)
+            print("updated weight:", self.W[i].shape)
+        
+            
         
     
     def sigmoid(self, x):
@@ -67,5 +79,14 @@ class NNClassifier():
     def relu(self, x):#checked
         return np.maximum(np.zeros((x.shape[0],x.shape[1])), x)
     
-    def predict_proba(self, x):
-        pass
+    def predict_proba(self, X):
+        #FORWARD PROPAGATION
+        self.Z[0] = X   #treat the input layer as Z[0]
+        self.H[0] = np.hstack((self.Z[0], column)) #add extra culumn
+        
+        for i in range(1, self.n_layers - 1):#Calculate hidden layers
+            self.Z[i] = np.dot(self.H[i-1], self.W[i-1])
+            self.H[i] = np.hstack((self.relu(self.Z[i]), column))
+        Yhat = np.dot(self.H[self.n_layers - 2], self.W[self.n_layers - 2])
+        Yhat = self.sigmoid(Yhat)
+        return Yhat
