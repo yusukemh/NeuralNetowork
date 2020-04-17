22652c13d0a0493c1aa01fff21627b690a10ebf3
Yusuke Hatanaka <yusukemh@hawaii.edu>
2020-04-16 20:42:39 -1000

    before faster calc

--- NN_Driver.py
+++ NN_Driver.py
@@ -1,18 +1,102 @@
+
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
 import numpy as np
-#import NN_Class
+import sklearn.metrics # Area Under the ROC calculations.
 import NeuralNetwork
-N = 100
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+'''
+
+x_tr = pd.read_csv('tox21_dense_train.csv')
+x_tr = np.array(x_tr.iloc[:,1:])
+scaler.fit(x_tr)
+x_tr = scaler.transform(x_tr)
+
+y_tr = pd.read_csv('tox21_labels_train.csv')
+y_tr = y_tr.fillna(0)
+y_tr = np.array(y_tr.loc[:,'NR.AR']) # Each Column is a type of toxicity.
+
+x_te = pd.read_csv('tox21_dense_test.csv')
+x_te = np.array(x_te.iloc[:,1:])
+x_te = scaler.transform(x_te) # Must use exact same preprocessing as train set.
+
+y_te = pd.read_csv('tox21_labels_test.csv')
+y_te = y_te.fillna(0)
+y_te = np.array(y_te.loc[:,'NR.AR']) 
+
+label = np.array(np.where(y_tr == 1)).flatten()
+label_2 = np.array(np.where(y_tr == 0)).flatten()
+a = np.concatenate((label, label_2[:383]))
+#new = y_tr[a]
+#x_tr = x_tr[a]
+#y_tr = y_tr[a]
+#print(label_2[0].shape)
+
+
+
+model = NeuralNetwork.NNClassifier([100,100,100], learning_rate = 0.01, batch_size = 200, epochs = 1000)
+x_tr = x_tr[:,:]
+y_tr = y_tr[:]
+
+x_tr = sklearn.preprocessing.normalize(x_tr)
+x_te = sklearn.preprocessing.normalize(x_te)
+model.fit(x_tr, y_tr)
+p_te = model.predict_proba(x_te)
+print(p_te[:100])
+auc_te = sklearn.metrics.roc_auc_score(y_te, p_te)
+yhat = p_te.flatten()
+print(np.sum(yhat>=0.5))
+print("Test set AUC: %3.5f" % (auc_te))
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+N = 10
 D = 5
-X = np.random.rand(N, D) # NxD matrix
-Y = np.zeros(N) # length N vector
+#X = np.random.rand(N, D) # NxD matrix
+#Y = np.zeros(N) # length N vector
 
-model = NeuralNetwork.NNClassifier([6,10])
+X = np.array([[1,1,0,1,1],
+              [1,1,0,1,1],
+              [1,1,0,0,1],
+              [1,1,1,1,0],
+              [1,0,1,1,1],
+              [0,0,1,0,0],
+              [0,0,1,1,0],
+              [0,1,0,1,0],
+              [0,0,1,0,1],
+              [0,0,0,1,1]])
+Y = np.array([[1],
+              [1],
+              [1],
+              [1],
+              [1],
+              [0],
+              [0],
+              [0],
+              [0],
+              [0]])
+
+model = NeuralNetwork.NNClassifier([50,50],learning_rate = 0.05, batch_size = 10, epochs = 1000)
 model.fit(X, Y)
-a = np.array([1,2,3])
-print(np.multiply(4,a))
+Yhat = model.predict_proba(X)
+print("Afer all...")
+print(Yhat)
 
 
 
-#model.printWeights()
-#print(len(model.getHiddenLayerSizes()))
-#print(model.getNumLayers())
+
+
+
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,11 +1,13 @@
 import numpy as np
-
+np.set_printoptions(precision=10)
 class NNClassifier():
-    def __init__(self, hidden_layer_sizes, learning_rate = 0.01):
+    def __init__(self, hidden_layer_sizes, learning_rate = 0.08, batch_size = 200, epochs = 100):
         self.hidden_layer_sizes = hidden_layer_sizes
         self.learning_rate = learning_rate
         self.n_hidden_layers = len(hidden_layer_sizes)
         self.n_layers = self.n_hidden_layers + 2
+        self.batch_size = batch_size
+        self.epochs = epochs
         self.W = {}
         self.Z = {}
         self.H = {}
@@ -18,57 +20,61 @@ class NNClassifier():
         #Output: labels y, a length N vector of 0s, 1s
         
         #preprocess
-        Y = Y.reshape(100,-1)
+        X_intact = X
+        Y_intact = Y.reshape(-1,1)
+
         #incorporate the input and output layers
         self.layer_sizes.append(X.shape[1])
         for size in self.hidden_layer_sizes:
-            self.layer_sizes.append(size)
+          self.layer_sizes.append(size)
         self.layer_sizes.append(1)
-        
         #initialize the weight randomly. Allow negative values
         for i in range(self.n_layers - 1):
-            self.W[i] = (np.random.rand(self.layer_sizes[i] + 1, self.layer_sizes[i+1]) - 0.5) / 100
-        
-        '''
-        for i in range(self.n_layers -1):
-            print(self.W[i].shape)
-            print(self.W[i])
-            print()
-        '''
-        
-        n = X.shape[0]
+          self.W[i] = (np.random.rand(self.layer_sizes[i] + 1, self.layer_sizes[i+1]) - 0.5) / 100
+          #self.W[i] = np.zeros((self.layer_sizes[i]+1, self.layer_sizes[i+1])) +  0.01
+          
+        iteration = int(X.shape[0] / self.batch_size)
+        n = self.batch_size
         column = np.ones((n,1))
         
-        #FORWARD PROPAGATION
-        self.Z[0] = X   #treat the input layer as Z[0]
-        self.H[0] = np.hstack((self.Z[0], column)) #add extra culumn
-        
-        for i in range(1, self.n_layers - 1):#Calculate hidden layers
-            self.Z[i] = np.dot(self.H[i-1], self.W[i-1])
-            self.H[i] = np.hstack((self.relu(self.Z[i]), column))
-        Yhat = np.dot(self.H[self.n_layers - 2], self.W[self.n_layers - 2])
-        Yhat = self.sigmoid(Yhat)
-        
-        
-        #BACK PROPAGATION
-        dLdz = Yhat - Y# (N, 1)
-        dLdw = np.dot(np.transpose(dLdz), self.H[self.n_layers - 2]) #(1,N) x (N, |H_top|)
-        
-        G = {}
-        G[self.n_layers - 2] = np.transpose(dLdw)
-        
-        for i in range(self.n_layers - 3, -1, -1): #n_layers - 3, n_layer - 4, ... 0
-            dLdz = np.dot(dLdz, np.transpose(self.W[i+1][:-1,:]))
-            dLdz = np.multiply(dLdz, self.relu(self.Z[i+1]))
-            dLdw = np.dot(np.transpose(dLdz), self.H[i])
-            G[i] = np.transpose(dLdw)
-        
-        #Use the gradient to update the weights
-        for i in range(self.n_layers-1):
-            print("updating weight:", self.W[i].shape)
-            self.W[i] = self.W[i] - np.multiply(self.learning_rate, G[i])
-            print(np.multiply(self.learning_rate, G[i]).shape)
-            print("updated weight:", self.W[i].shape)
+        for epoch in range(self.epochs):
+          start = 0
+          end = self.batch_size
+          
+          #G = {}
+          for step in range(iteration):
+            X_batch = X_intact[start:end, :]
+            Y_batch = Y_intact[start:end, :]
+            
+            #FORWARD PROPAGATION
+            self.Z[0] = X_batch   #treat the input layer as Z[0]
+            self.H[0] = np.hstack((self.Z[0], column)) #add extra culumn
+            for i in range(1, self.n_layers - 1):#Calculate hidden layers
+              self.Z[i] = np.dot(self.H[i-1], self.W[i-1])
+              self.H[i] = np.hstack((self.relu(self.Z[i]), column))
+            Yhat = np.dot(self.H[self.n_layers - 2], self.W[self.n_layers - 2])
+            Yhat = self.sigmoid(Yhat)
+            
+            #BACK PROPAGATION
+            dLdz = Yhat - Y_batch# (N, 1)
+            dLdw = np.dot(np.transpose(dLdz), self.H[self.n_layers - 2]) #(1, -1)
+            #update the layer
+            #self.W[self.n_layers - 2] -= np.transpose(dLdw)
+            G = {}
+            G[self.n_layers - 2] = np.transpose(dLdw)      
+            for i in range(self.n_layers - 3, -1, -1): #n_layers - 3, n_layer - 4, ... 0
+              dLdz = np.dot(dLdz, np.transpose(self.W[i+1][:-1,:]))
+              dLdz = np.multiply(dLdz, self.reluD(self.Z[i+1]))
+              dLdw = np.dot(np.transpose(dLdz), self.H[i])
+              G[i] = np.transpose(dLdw)
+
+
+            #Use the gradient to update the weights
+            for i in range(self.n_layers -1):
+              self.W[i] = np.subtract(self.W[i], np.multiply(self.learning_rate, G[i]))
+
+            start += self.batch_size
+            end += self.batch_size
         
             
         
@@ -79,8 +85,13 @@ class NNClassifier():
     def relu(self, x):#checked
         return np.maximum(np.zeros((x.shape[0],x.shape[1])), x)
     
+    def reluD(self, x):
+        return np.maximum(np.zeros((x.shape[0], x.shape[1])), 1)
+    
     def predict_proba(self, X):
         #FORWARD PROPAGATION
+        n = X.shape[0]
+        column = np.ones((n,1))
         self.Z[0] = X   #treat the input layer as Z[0]
         self.H[0] = np.hstack((self.Z[0], column)) #add extra culumn
         
